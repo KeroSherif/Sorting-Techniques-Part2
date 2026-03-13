533e745253d4a2b4b00b993d03de4744dc0c83a4
KeroSherif <kerosherif8@gmail.com>
2026-03-13 17:02:08 +0200

    ADD QUICK SORT

--- Sorting_Techniques_part2.py
+++ Sorting_Techniques_part2.py
@@ -48,6 +48,26 @@ def merge_sort(arr):
     right = merge_sort(arr[mid:])
     return merge(left, right)
 
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i + 1], array[high] = array[high], array[i + 1]
+    return i + 1
+
+def quicksort(array, low=0, high=None):
+    if high is None:
+        high = len(array) - 1
+
+    if low < high:
+        pivot_index = partition(array, low, high)
+        quicksort(array, low, pivot_index - 1)
+        quicksort(array, pivot_index + 1, high)
 
 def heap_sort(arr):
     
@@ -96,6 +116,7 @@ if __name__ == "__main__":
         heap_arr=original_arr.copy()
         merge_arr=original_arr.copy()
         hybrid_merge_arr=original_arr.copy()
+        quick_arr=original_arr.copy()
 
         # Bubble Sort
         start_time = time.time()
@@ -118,19 +139,26 @@ if __name__ == "__main__":
         duration = (end_time - start_time) * 1000
         print(f"Running time for Insertion Sort is {duration:.2f} ms")
         
-        #heap sort
-        start_time = time.time()
-        heap_sort(heap_arr)
-        end_time = time.time()
-        duration = (end_time - start_time) * 1000
-        print(f"Running time for heap Sort is {duration:.2f} ms")
-        
         #Merge sort
         start_time = time.time()
         merge_sort(merge_arr)
         end_time = time.time()
         duration = (end_time - start_time) * 1000
         print(f"Running time for merge Sort is {duration:.2f} ms")
+        
+        #Quick sort
+        start_time = time.time()
+        quicksort(quick_arr)
+        end_time = time.time()
+        duration = (end_time - start_time) * 1000
+        print(f"Running time for Quick Sort is {duration:.2f} ms")
+        
+        #heap sort
+        start_time = time.time()
+        heap_sort(heap_arr)
+        end_time = time.time()
+        duration = (end_time - start_time) * 1000
+        print(f"Running time for heap Sort is {duration:.2f} ms")
         
         #Hyprid Merge sort
         start_time = time.time()
